566676a3d4d4b3cd2b470a4e6d1fca2cdfb9a44a
dilarangoding <riyonnaryono@gmail.com>
2025-04-10 11:31:33 +0700

    chore:init

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const posts = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    image: z.string().default("/static/og.png"),
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collections = { posts };
